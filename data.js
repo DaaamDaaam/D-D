e99d6112c4ffca4a839e23e1fa86633b8ad81bc6
Miklovicova Davy <21801674t>
2020-02-19 13:43:56 +0100

    fonction getTheme / GetQuiz

--- data.js
+++ data.js
@@ -1,14 +1,22 @@
-const liste_theme = [
+export const liste_theme = [
     {
         id : 1,
         titre:'Thème 1'
     }
 ]
 
-const liste_quiz = [
+export const liste_quiz = [
     {
         id : 1,
         titre: 'Quiz 1',
         id_theme: 1
     }
 ]
+
+export function getTheme(id) {
+    return listeTheme.find(item => item.id === id);
+  }
+  
+export function getQuiz(id) {
+    return listeQuiz.find(item => item.id === id);
+  }
