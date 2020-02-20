72c9e5f57bfcd6d2160052069c73039c12657e21
Legros Damien <21801722t>
2020-02-20 15:02:27 +0100

    quiz.vue

---
+++ src/components/Quiz.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>Quiz : {{ idQuiz }}</div>
+</template>
+<script>
+export default {
+    name: "Quiz",
+    props: ["idQuiz"],
+    data: function() {
+        return {};
+    },
+    computed: {
+        quiz: function() {
+            return getQuizAjax(this.idQuiz);
+        }
+    }
+}
+</script>
